9b3da72dd118a569cc23ccc4b0d4716fa0a71b50
FunatsuMasashi <m_funatsu@pignusinc.onmicrosoft.com>
2024-10-17 00:34:40 +0900

    InitialModal.tsxを分割_修正

--- src/components/ui/LocationInput.tsx
+++ src/components/ui/LocationInput.tsx
@@ -2,8 +2,14 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
 // register の型を適切に指定するためのインターフェースを定義
+interface RegisterReturn {
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur: (event: React.FocusEvent<HTMLInputElement>) => void;
+  name: string;
+}
+
 interface LocationInputProps {
-  register: (name: string) => { [key: string]: any }; // オブジェクトを返すように型を指定
+  register: (name: string) => RegisterReturn; // より具体的な型を指定
 }
 
 export function LocationInput({ register }: LocationInputProps) {
