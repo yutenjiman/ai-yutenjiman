fabdcd23aee7812065fa021990e0be9a5d7e4759
FunatsuMasashi <m_funatsu@pignusinc.onmicrosoft.com>
2024-10-17 11:23:51 +0900

    CuisineSelectのモバイル挙動を修正

--- src/components/ui/CuisineSelect.tsx
+++ src/components/ui/CuisineSelect.tsx
@@ -1,27 +1,13 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState } from 'react';
 
 export function CuisineSelect({ setValue, cuisines }: { setValue: (key: string, value: string) => void, cuisines: { label: string, value: string }[] }) {
   const [cuisineSearch, setCuisineSearch] = useState('');
   const inputRef = useRef<HTMLInputElement>(null);
 
   const filteredCuisines = cuisines.filter(cuisine => cuisine.label.includes(cuisineSearch));
-
-  useEffect(() => {
-    const handleTouchStart = (e: TouchEvent) => {
-      if (inputRef.current && !inputRef.current.contains(e.target as Node)) {
-        e.preventDefault();
-      }
-    };
-
-    document.addEventListener('touchstart', handleTouchStart, { passive: false });
-
-    return () => {
-      document.removeEventListener('touchstart', handleTouchStart);
-    };
-  }, []);
 
   const handleSelectTriggerClick = () => {
     if (!/Mobi|Android/i.test(navigator.userAgent)) {
