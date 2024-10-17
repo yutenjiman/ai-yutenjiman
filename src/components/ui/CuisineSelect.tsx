fc937492e8c58c08096dcf274e47db2858887827
FunatsuMasashi <m_funatsu@pignusinc.onmicrosoft.com>
2024-10-17 13:55:33 +0900

    CuisineSelectのモバイル挙動を修正

--- src/components/ui/CuisineSelect.tsx
+++ src/components/ui/CuisineSelect.tsx
@@ -8,50 +8,48 @@ export function CuisineSelect({ setValue, cuisines }: { setValue: (key: string, 
   const inputRef = useRef<HTMLInputElement>(null);
   const [isOpen, setIsOpen] = useState(false);
 
-  const filteredCuisines = cuisines.filter(cuisine => cuisine.label.includes(cuisineSearch));
+  const filteredCuisines = cuisines.filter(cuisine => cuisine.label.toLowerCase().includes(cuisineSearch.toLowerCase()));
 
   const handleSelectTriggerClick = () => {
     setIsOpen(!isOpen);
   };
 
   const handleInputFocus = () => {
-    setIsOpen(true); // フォーカス時にドロップダウンを開く
+    setIsOpen(true);
   };
 
-  const handleInputBlur = () => {
-    // タイムアウトを利用して次のクリックが発生するかどうかを少し待つ
-    setTimeout(() => {
-      if (!document.activeElement?.closest('.select-content')) {
-        setIsOpen(false); // フォーカスが完全に外れたらドロップダウンを閉じる
-      }
-    }, 100); // 小さな遅延を加える
+  const handleInputBlur = (e: React.FocusEvent) => {
+    // リレーテッドターゲットがSelectContent内にある場合は閉じない
+    if (!e.relatedTarget || !e.currentTarget.contains(e.relatedTarget as Node)) {
+      setTimeout(() => {
+        setIsOpen(false);
+      }, 200);
+    }
   };
 
   return (
     <div>
       <Label htmlFor="cuisine">料理ジャンル（任意）</Label>
-      <Select onValueChange={(value) => setValue('cuisine', value)}>
+      <Select onValueChange={(value) => setValue('cuisine', value)} open={isOpen} onOpenChange={setIsOpen}>
         <SelectTrigger onClick={handleSelectTriggerClick}>
           <SelectValue placeholder="料理ジャンルを選択" />
         </SelectTrigger>
-        {isOpen && (
-          <SelectContent className="select-content">
-            <Input
-              ref={inputRef}
-              placeholder="料理ジャンルを検索"
-              value={cuisineSearch}
-              onChange={(e) => setCuisineSearch(e.target.value)}
-              className="mb-2"
-              onFocus={handleInputFocus}
-              onBlur={handleInputBlur}
-            />
-            {filteredCuisines.map(cuisine => (
-              <SelectItem key={cuisine.value} value={cuisine.value}>
-                {cuisine.label}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        )}
+        <SelectContent className="select-content">
+          <Input
+            ref={inputRef}
+            placeholder="料理ジャンルを検索"
+            value={cuisineSearch}
+            onChange={(e) => setCuisineSearch(e.target.value)}
+            className="mb-2"
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+          />
+          {filteredCuisines.map(cuisine => (
+            <SelectItem key={cuisine.value} value={cuisine.value}>
+              {cuisine.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
       </Select>
     </div>
   );
